3eb4959db13d346abc63de2bbf1c5e57068d4e25
Alek30k <alemank23k@gmail.com>
2023-02-07 12:43:27 -0300

    Last commit

--- tipos de datos/datos-compuestos.py
+++ tipos de datos/datos-compuestos.py
@@ -2,6 +2,6 @@ lista = ["Ale", "Pepito", "Maria",True,1.23] #se pueden modificar
 tupla = ("Ale", "Pepito", "Maria",True,1.23) #no se pueden modificar
 print(tupla[2])
 
-#creando un conjunto (set) son elementos desordenados pero tampoco de puede modificar pero si redefinir
+#creando un conjunto (set) son elementos desordenados, no puede haber datos duplicados,tampoco se puede modificar pero si redefinir
 
 conjunto = {"Ale", "Pepito", "Maria",True,1.23}
