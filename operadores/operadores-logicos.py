3704a463d75ac0faf87a6836889f8def89019899
Alek30k <alemank23k@gmail.com>
2023-02-11 12:04:44 -0300

    last commit

---
+++ operadores/operadores-logicos.py
@@ -0,0 +1,18 @@
+#AND
+
+Res = True & True #Devolver True
+Res = False & True #Devolver False
+Res = True & False #Devolver False
+Res = False & False #Devolver False
+
+#OR
+
+Res = True | True #Devolver True
+Res = False | True #Devolver True
+Res = True | False #Devolver True
+Res = False | False #Devolver False
+
+#NOT
+
+Res = not True #Devolver False
+Res = not False #Devolver True
