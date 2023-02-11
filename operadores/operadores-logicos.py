98d0a3c391878ce80e52419803bcf58b591fef63
Alek30k <alemank23k@gmail.com>
2023-02-11 12:05:25 -0300

    last commit

--- operadores/operadores-logicos.py
+++ operadores/operadores-logicos.py
@@ -1,18 +1,18 @@
 #AND
 
-Res = True & True #Devolver True
-Res = False & True #Devolver False
-Res = True & False #Devolver False
-Res = False & False #Devolver False
+Res1 = True & True #Devolver True
+Res2 = False & True #Devolver False
+Res3 = True & False #Devolver False
+Res4 = False & False #Devolver False
 
 #OR
 
-Res = True | True #Devolver True
-Res = False | True #Devolver True
-Res = True | False #Devolver True
-Res = False | False #Devolver False
+Res5 = True | True #Devolver True
+Res6 = False | True #Devolver True
+Res7 = True | False #Devolver True
+Res8 = False | False #Devolver False
 
 #NOT
 
-Res = not True #Devolver False
-Res = not False #Devolver True
+Res9 = not True #Devolver False
+Res10 = not False #Devolver True
