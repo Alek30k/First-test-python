e55705dcc77867651e7fe002bc44bdcc6fdb0723
Alek30k <alemank23k@gmail.com>
2023-02-12 10:07:47 -0300

    last commit

--- Ejercicios/ejercicios-1.1.py
+++ Ejercicios/ejercicios-1.1.py
@@ -9,7 +9,9 @@ dalto_curso = 1.5
 #diferencias de duracion
 
 diferencia_con_min = 100 - dalto_curso / otros_cursos_min * 100
-diferencia_con_max = 100 - dalto_curso / otros_cursos_max * 100
+diferencia_con_max = 100 - dalto_curso // otros_cursos_max * 100
 diferencia_con_promedio = 100 - dalto_curso / otros_cursos_promedio * 100
 
 print(f'El curso de Dalto dura un {diferencia_con_min}% menos que el más rápido')
+print(f'El curso de Dalto dura un {diferencia_con_max}% menos que el más lento')
+print(f'El curso de Dalto dura un {diferencia_con_promedio}% menos que el promedio')
